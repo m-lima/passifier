d9f2044cdbe39818aed748f7068fc9c9f3a4fe11
Marcelo Lima <marcelowind@gmail.com>
2021-02-21 15:08:55 +0100

    Simplify

--- src/lib.rs
+++ src/lib.rs
@@ -3,37 +3,6 @@
 //! Handles secrets in a secret store
 
 mod crypter;
-mod store;
-
-macro_rules! impl_store {
-    ($name:ty) => {
-        impl Store for $name {
-            fn create(&mut self, name: String, value: String) -> Result<()> {
-                self.store.create(name, value)
-            }
-
-            fn read<K: AsRef<str>>(&self, name: K) -> Option<&String> {
-                self.store.read(name)
-            }
-
-            fn update<K: AsRef<str>>(&mut self, name: K, value: String) -> Result<()> {
-                self.store.update(name, value)
-            }
-
-            fn delete<K: AsRef<str>>(&mut self, name: K) -> Result<String> {
-                self.store.delete(name)
-            }
-
-            fn secrets(&self) -> SecretNames<'_> {
-                self.store.secrets()
-            }
-
-            fn iter(&self) -> Secrets<'_> {
-                self.store.iter()
-            }
-        }
-    };
-}
 
 /// Conveniene alias for operations that may return an [`Error`](enum.Error.html)
 pub type Result<T> = std::result::Result<T, Error>;
@@ -58,107 +27,224 @@ pub enum Error {
     SecretNotFound,
 }
 
-/// Trait for all implementaions of a secrets store
-pub trait Store {
+/// Possible backends for the secret store
+pub enum Backend {
+    /// File based secret store
+    File(std::path::PathBuf),
+}
+
+impl Backend {
+    fn load(&self) -> Result<Vec<u8>> {
+        match self {
+            Self::File(path) => {
+                use std::io::Read;
+
+                let mut file = std::fs::File::open(path).map_err(Error::IO)?;
+                let mut buffer = Vec::new();
+                file.read_to_end(&mut buffer).map_err(Error::IO)?;
+                Ok(buffer)
+            }
+        }
+    }
+
+    fn save(&self, buffer: &[u8]) -> Result<()> {
+        match self {
+            Self::File(path) => {
+                use std::io::Write;
+
+                std::fs::File::create(path)
+                    .map_err(Error::IO)?
+                    .write_all(buffer)
+                    .map_err(Error::IO)
+                    .map(|_| ())
+            }
+        }
+    }
+}
+
+/// Yo
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct Store(std::collections::HashMap<String, String>);
+
+impl Store {
+    /// Load a secret store from the backend with the given passphrase
+    ///
+    /// # Errors
+    /// Any IO failure will result in an error. After loading the bytes, any decryption,
+    /// deserialization, and decrompression failures will result in an error.
+    pub fn load<S: AsRef<str>>(backend: &Backend, pass: S) -> Result<Self> {
+        let buffer = backend.load()?;
+        crypter::Crypter::new(pass)
+            .decrypt(&buffer)
+            .map_err(Error::Crypto)
+    }
+
+    /// Save the secret store back into the backend with the given passphrase
+    ///
+    /// # Errors
+    /// Any encryption, serialization, and crompression failures will result in an error. After
+    /// preparing the payload, any IO failure will result in an error.
+    pub fn save<S: AsRef<str>>(&self, backend: &Backend, pass: S) -> Result<()> {
+        let buffer = crypter::Crypter::new(pass)
+            .encrypt(self)
+            .map_err(Error::Crypto)?;
+
+        backend.save(&buffer)
+    }
+
     /// Creates a new secret in the store
     ///
     /// # Errors
     /// If the secret name already exists, a
     /// [`SecretAlreadyExists`](enum.Error.html#variant.SecretAlreadyExists) error will be returned
-    fn create(&mut self, name: String, value: String) -> Result<()>;
+    pub fn create(&mut self, name: String, value: String) -> Result<()> {
+        match self.0.entry(name) {
+            std::collections::hash_map::Entry::Vacant(entry) => {
+                entry.insert(value);
+                Ok(())
+            }
+            std::collections::hash_map::Entry::Occupied(_) => Err(Error::SecretAlreadyExists),
+        }
+    }
 
     /// Reads a secret from the store, if it exists
-    fn read<S: AsRef<str>>(&self, name: S) -> Option<&String>;
+    pub fn read<S: AsRef<str>>(&self, name: S) -> Option<&String> {
+        self.0.get(name.as_ref())
+    }
 
     /// Updates a secret from the store
     ///
     /// # Errors
     /// If the secret name does not exist, a
     /// [`SecretNotFound`](enum.Error.html#variant.SecretNotFound) error will be returned
-    fn update<S: AsRef<str>>(&mut self, name: S, value: String) -> Result<()>;
+    pub fn update<S: AsRef<str>>(&mut self, name: S, value: String) -> Result<()> {
+        let entry = self.0.get_mut(name.as_ref()).ok_or(Error::SecretNotFound)?;
+        *entry = value;
+        Ok(())
+    }
 
     /// Deletes a secret from the store
     ///
     /// # Errors
     /// If the secret name does not exist, a
     /// [`SecretNotFound`](enum.Error.html#variant.SecretNotFound) error will be returned
-    fn delete<S: AsRef<str>>(&mut self, name: S) -> Result<String>;
+    pub fn delete<S: AsRef<str>>(&mut self, name: S) -> Result<String> {
+        self.0.remove(name.as_ref()).ok_or(Error::SecretNotFound)
+    }
 
     /// An iterator over all the secret names stored
-    fn secrets(&self) -> SecretNames<'_>;
+    pub fn secrets(&self) -> impl Iterator<Item = &String> {
+        self.0.keys()
+    }
 
     /// An iterator over all the secret name/value pairs
-    fn iter(&self) -> Secrets<'_>;
-}
-
-/// Iterator over secret names
-pub struct SecretNames<'a>(std::collections::hash_map::Keys<'a, String, String>);
-
-impl<'a> Iterator for SecretNames<'a> {
-    type Item = &'a String;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.next()
+    pub fn iter(&self) -> impl Iterator<Item = (&String, &String)> {
+        self.0.iter()
     }
 }
 
-/// Iterator over secret name/value pais
-pub struct Secrets<'a>(std::collections::hash_map::Iter<'a, String, String>);
+#[cfg(test)]
+mod tests {
 
-impl<'a> Iterator for Secrets<'a> {
-    type Item = (&'a String, &'a String);
+    mod backend {}
 
-    fn next(&mut self) -> Option<Self::Item> {
-        self.0.next()
-    }
-}
+    mod store {
+        use super::super::Error;
+        use super::super::Store;
 
-/// Implementation of a secret store that is file-based
-pub struct FileStore {
-    path: std::path::PathBuf,
-    store: store::Store,
-}
+        macro_rules! own {
+            ($string:literal) => {
+                String::from($string)
+            };
+        }
 
-impl_store!(FileStore);
+        fn setup() -> (Store, std::collections::HashMap<String, String>) {
+            let mut reference = std::collections::HashMap::new();
+            reference.insert(own!("existing"), own!("existing_value"));
 
-impl FileStore {
-    /// Loads a secret store from file
-    ///
-    /// # Errors
-    /// Any IO failure will result in an error. After reading the file, any decryption,
-    /// deserialization, and decrompression failures will result in an error.
-    pub fn load<P: AsRef<std::path::Path>, S: AsRef<str>>(path: P, pass: S) -> Result<Self> {
-        use std::io::Read;
+            let store = Store(reference.clone());
 
-        let mut file = std::fs::File::open(path.as_ref()).map_err(Error::IO)?;
-        let mut buffer = Vec::new();
-        file.read_to_end(&mut buffer).map_err(Error::IO)?;
+            (store, reference)
+        }
 
-        let store = crypter::Crypter::new(pass)
-            .decrypt(&buffer)
-            .map_err(Error::Crypto)?;
-        Ok(Self {
-            path: std::path::PathBuf::from(path.as_ref()),
-            store,
-        })
-    }
+        #[test]
+        fn create() {
+            let (mut store, mut reference) = setup();
 
-    /// Save the secret store back into file
-    ///
-    /// # Errors
-    /// Any encryption, serialization, and crompression failures will result in an error. After
-    /// preparing the payload, any IO failure will result in an error.
-    pub fn save<S: AsRef<str>>(self, pass: S) -> Result<()> {
-        use std::io::Write;
+            assert!(matches!(
+                store
+                    .create(own!("existing"), own!("existing_new_value"))
+                    .unwrap_err(),
+                Error::SecretAlreadyExists
+            ));
+            assert_eq!(store.0, reference);
 
-        let buffer = crypter::Crypter::new(pass)
-            .encrypt(&self.store)
-            .map_err(Error::Crypto)?;
+            assert!(store.create(own!("new"), own!("new_value")).is_ok());
+            reference.insert(own!("new"), own!("new_value"));
+            assert_eq!(store.0, reference);
 
-        std::fs::File::create(self.path)
-            .map_err(Error::IO)?
-            .write_all(&buffer)
-            .map_err(Error::IO)
-            .map(|_| ())
+            assert!(matches!(
+                store
+                    .create(own!("new"), own!("new_new_value"))
+                    .unwrap_err(),
+                Error::SecretAlreadyExists
+            ));
+            assert_eq!(store.0, reference);
+        }
+
+        #[test]
+        fn read() {
+            let (store, reference) = setup();
+            assert!(store.read("new").is_none());
+            assert_eq!(store.0, reference);
+            assert_eq!(store.read("existing").unwrap(), "existing_value");
+            assert_eq!(store.0, reference);
+        }
+
+        #[test]
+        fn update() {
+            let (mut store, mut reference) = setup();
+
+            assert!(matches!(
+                store.update("new", own!("new_value")).unwrap_err(),
+                Error::SecretNotFound
+            ));
+            assert_eq!(store.0, reference);
+
+            assert!(store.update("existing", own!("new_value")).is_ok());
+            reference.insert(own!("existing"), own!("new_value"));
+            assert_eq!(store.0, reference);
+        }
+
+        #[test]
+        fn delete() {
+            let (mut store, mut reference) = setup();
+
+            assert!(matches!(
+                store.delete("new").unwrap_err(),
+                Error::SecretNotFound
+            ));
+            assert_eq!(store.0, reference);
+
+            assert!(store.delete("existing").is_ok());
+            reference.clear();
+            assert_eq!(store.0, reference);
+        }
+
+        #[test]
+        fn secrets() {
+            let store = {
+                let mut map = std::collections::HashMap::new();
+                map.insert(own!("foo1"), own!("bar1"));
+                map.insert(own!("foo2"), own!("bar2"));
+                map.insert(own!("foo3"), own!("bar3"));
+                Store(map)
+            };
+            let list = store.secrets().collect::<Vec<_>>();
+            assert_eq!(list.len(), 3);
+            assert!(list.contains(&&own!("foo1")));
+            assert!(list.contains(&&own!("foo2")));
+            assert!(list.contains(&&own!("foo3")));
+        }
     }
 }
